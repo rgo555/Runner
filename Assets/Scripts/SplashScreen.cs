93557e00151bd00b030a4c54dc8bf1f0654719b3
VicCTS <victordm.cts@gmail.com>
2022-10-21 12:40:54 +0200

    splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public float time;
+    Scene scene;
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        Invoke("LoadMainMenu", time);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+
+    
+}
